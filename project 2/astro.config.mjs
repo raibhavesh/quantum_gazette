9bd3f6bc84f39c6e4a436f2fb20e36a8257bb2d4
Bhavesh Rai <113577193+raibhavesh@users.noreply.github.com>
2024-12-13 23:29:26 +0530

    Delete project 2 directory

--- project 2/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  output: 'static',
-  site: 'https://quantum-gazette.com',
-  devToolbar: {
-    enabled: false,
-  },
-});
